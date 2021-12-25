48760a0cf47504572969949e4683ef75a154c16b
Agung Budi Santoso <santosobudiagung9@gmail.com>
2021-12-25 12:46:24 +0700

    [First Commit] Set Dependencies

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+// Update with your config settings.
+
+module.exports = {
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+};
